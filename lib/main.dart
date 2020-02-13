f491e02a80cd1fc7fdb9abda20ef301eeca63295
kkunal42@gmail.com <kkunal42@gmail.com>
2020-02-13 13:17:26 +0530

    workshop d2

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
-// TODO: In day 2 of workshop
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+   Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+       appBar: AppBar(title: Text("hello world"),),
+       body: Column(
+         children:<Widget>[
+           Text("Hello world")
+         ],
+       ),
+      ),
+    );
+  }
+}
